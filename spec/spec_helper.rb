523cbc4bd83c63453746f7b563e6023f4615e261
Mike Herrera <michael.herrera@gmail.com>
2015-04-30 13:12:01 -0700

    Removed unnecessary rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -61,10 +61,6 @@ Dir[File.expand_path(File.join(File.dirname(__FILE__),'factories','**','*.rb'))]
 Dir["./spec/support/**/*.rb"].sort.each {|f| require f}
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
